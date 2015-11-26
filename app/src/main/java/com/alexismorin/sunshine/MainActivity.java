4747820f47c301bac0c46c3b23ea6d98a50c45ca
Alexis Morin <carignan.boy@gmail.com>
2015-11-26 23:11:02 +0800

    Only have the dual-pane layout trigger on 7-inch and above in landscape

--- app/src/main/java/com/alexismorin/sunshine/MainActivity.java
+++ app/src/main/java/com/alexismorin/sunshine/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
 
         if(findViewById(R.id.weather_detail_container) != null){
             //The detail container view will be present only in the large-screen layouts
-            // (res/layout-sw600dp). If this view is present, then the activity should be
+            // (res/layout-sw600dp-land). If this view is present, then the activity should be
             // in two-pane mode.
             mTwoPane = true;
 
